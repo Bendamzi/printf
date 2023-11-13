b550728ab657cb4d5b9c30c368986df971f03fb6
idowujoshua1 <jonlaney346@gmail.com>
2023-11-13 03:08:42 -0800

    0-printf.c

---
+++ 0-printf.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+int _printf(const char *format, ...)
+{
+	va_list args_list;
+	va_start(args_list, format);
+
+	int count = 0;
+
+	while (*format != '\0')
+	{
+		if (*format == '%')
+		{
+			format++
+			switch (*format)
+			{
+			case 'c';
+			count += putchar(va_arg(args_list, int));
+			break;
+			case 's':
+			count += strlen("%s", va_arg(args_list, char *));
+			break;
+			case '%';
+			count += putchar('%');
+			count += putchar(*format);
+			}
+		}
+		else 
+		{
+			count += putchar(*format);
+		}
+		format++;
+	}
+
+
+	va_end(args_list)
+	return count;
+}
--- main.h
+++ main.h
@@ -3,5 +3,8 @@
 #include <stdarg.h>
 #include <string.h>
 #include <unistd.h>
+#include <stdio.h>
 int _printf(const char *format, ...);
+int _putchar(char c);
+
 #endif
