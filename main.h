9b26b70afdc46a301aa5621a7665d89a32cb049b
Bendamzi <okunsanyabenjamin40@gmail.com>
2023-11-17 15:39:45 -0800

    My commit

---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdarg.h>
+int _printf(const char *format, ...);
+int _putchar(char c);
+int check_specifier(const char *format, va_list args);
+int print_decimal(int num);
+int print_int(int num);
+#endif
