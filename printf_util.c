b209ef57ab2e4dcd54aa5d96bc3ef3f84496f0b5
Bendamzi <okunsanyabenjamin40@gmail.com>
2023-11-17 12:43:31 -0800

    My commit

--- printf_util.c
+++ printf_util.c
@@ -25,16 +25,11 @@ int is_printable(char c)
 
 int append_hexa_code(char ascii_code, char buffer[], int i)
 {
-	char map_to[] = "0123456789ABCDEF";
-
-	if (ascii_code < 0)
-		ascii_code *= -1;
-
 	buffer[i++] = '\\';
 	buffer[i++] = 'x';
 
-	buffer[i++] = map_to[ascii_code / 16];
-	buffer[i] = map_to[ascii_code % 16];
+	buffer[i++] = "0123456789ABCDEF"[ascii_code >> 4];
+	buffer[i] = "0123456789ABCDEF"[ascii_code & 0xF];
 
 	return (3);
 }
